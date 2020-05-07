506cf5311e7094129ef4bba8b1476271e9311dda
Sachin Patel <sachinpatel2707@gmail.com>
2020-05-07 16:23:13 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'omnifood';
+
+  navbar: boolean = false
+  
+  toggleNavBar() {
+    if(this.navbar) {
+      this.navbar = false
+    } else {
+      this.navbar = true
+    }
+  }
 }
